a773d1eb2adabbc857c4f41915b246c85304c0d5
mschamber <schambermichael1995@gmail.com>
2022-01-27 13:51:21 +0100

    ExO v2 Anmeldung

--- Project/PowerShell Project/Form1.cs
+++ Project/PowerShell Project/Form1.cs
@@ -10,47 +10,142 @@ using System.Windows.Forms;
 using System.Management.Automation;
 using System.Collections.ObjectModel;
 using System.Security;
+using System.Management.Automation.Runspaces;
+using System.Management.Automation.Remoting;
 
 namespace PowerShell_Project
 {
     public partial class Form1 : Form
     {
-        PowerShell ps;
         public Form1()
         {
             InitializeComponent();
-            ps = PowerShell.Create();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
         }
+        public static void WriteStreams(PSDataStreams Streams)
+        {
+            // Write Error, Warning and Information stream
 
+            PSDataCollection<ErrorRecord> errorStream = Streams.Error;
+            foreach (ErrorRecord errorRecord in errorStream)
+            {
+                Console.WriteLine(errorRecord.ToString());
+            }
+
+            PSDataCollection<WarningRecord> warningRecords = Streams.Warning;
+            foreach (WarningRecord warningRecord in warningRecords)
+            {
+                Console.WriteLine(warningRecord.ToString());
+            }
+
+            PSDataCollection<InformationRecord> informationRecords = Streams.Information;
+            foreach (InformationRecord informationRecord in informationRecords)
+            {
+                Console.WriteLine(informationRecord.ToString());
+            }
+
+            Console.WriteLine("");
+        }
         private void ConnectEO()
         {
-            string userName = "michael.schamber@nrz5.onmicrosoft.com";
-            string password = "Dpskdsb5771!";
-            SecureString secureString = new SecureString();
-            foreach (var item in password)
+
+            Collection<PSObject> results;
+
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(RunspaceConfiguration.Create()))
             {
-                secureString.AppendChar(item);
+                // Open local runspace
+                runspace.Open();
+
+                // Load Exchange Online PowerShell V2 module
+                using (PowerShell shell = PowerShell.Create())
+                {
+                    Console.WriteLine("Running : Import-Module ExchangeOnlineManagement");
+
+                    shell.Runspace = runspace;
+                    shell.Commands.AddScript("Import-Module ExchangeOnlineManagement");
+                    shell.Invoke();
+
+                    WriteStreams(shell.Streams);
+                }
+
+                // Connect to Exchange Online
+                ConnectExchangeOnlineUsingUserPrinsipalNameAndPassword(runspace);
+                //ConnectExchangeOnlineUsingPfxFile(runspace);
+                //ConnectExchangeOnlineUsingThumbprint(runspace);
+                //ConnectExchangeOnlineUsingX509Certificate2(runspace);
+
+                // Run Get-Mailbox
+                using (PowerShell shell = PowerShell.Create())
+                {
+                    Console.WriteLine("Running : Get-Mailbox -ResultSize Unlimited");
+
+                    shell.Runspace = runspace;
+                    shell.AddScript("Get-Mailbox -ResultSize Unlimited");
+
+                    results = shell.Invoke();
+
+                    foreach (PSObject result in results)
+                    {
+                        Console.WriteLine(result.Properties["UserPrincipalName"].Value.ToString());
+                    }
+
+                    WriteStreams(shell.Streams);
+                }
+                // Disconnect from Exchange Online
+                using (PowerShell shell = PowerShell.Create())
+                {
+                    Console.WriteLine("Running : Disconnect-ExchangeOnline -Confirm:$false");
+
+                    shell.Runspace = runspace;
+                    shell.AddScript("Disconnect-ExchangeOnline -Confirm:$false");
+
+                    results = shell.Invoke();
+                    foreach (PSObject result in results)
+                    {
+                        Console.WriteLine(result.Properties["UserPrincipalName"].Value.ToString());
+                    }
+
+                    WriteStreams(shell.Streams);
+                }
             }
-            secureString.MakeReadOnly();
-
-            PSCredential credential = new PSCredential(userName, secureString);
-
-            ps.AddCommand("Set-ExecutionPolicy")
-                .AddParameter("ExecutionPolicy","RemoteSigned")
-                .AddParameter("Scope","LocalMachine")
-                .Invoke();
-            ps.AddCommand("Import-Module").AddParameter("Name","ExchangeOnlineManagement").Invoke();
-            var result = ps.AddCommand("Connect-ExchangeOnline").Invoke();
-            var mailboxes = ps.AddCommand("Get-Mailbox").Invoke();
-
         }
+
+        private void ConnectExchangeOnlineUsingUserPrinsipalNameAndPassword(Runspace runspace)
+        {
+            using (PowerShell shell = PowerShell.Create())
+            {
+                Console.WriteLine("Running : Connect-ExchangeOnline -Credential <Cred> -ShowProgress $false -ShowBanner $false");
+
+                string userPrincipalName = "michael.schamber@nrz5.onmicrosoft.com";
+                string password = "Dpskdsb5771!";
+
+                SecureString secureString = new SecureString();
+
+                foreach (var ch in password)
+                {
+                    secureString.AppendChar(ch);
+                }
+
+                PSCredential pSCredential = new PSCredential(userPrincipalName, secureString);
+
+                shell.Runspace = runspace;
+                shell.Commands.AddCommand("Connect-ExchangeOnline");
+                shell.Commands.AddParameter("Credential", pSCredential);
+                shell.Commands.AddParameter("ShowProgress", false);
+                shell.Commands.AddParameter("ShowBanner", false);
+               // shell.Commands.AddParameter("PSSessionOption", new PSSessionOption() { ProxyAccessType = ProxyAccessType.IEConfig });
+
+                shell.Invoke();
+
+                WriteStreams(shell.Streams);
+            }
+        }
+
         private void DisconnectEO()
         {
-            ps.AddCommand("Disconnect-ExchangeOnline").Invoke();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
